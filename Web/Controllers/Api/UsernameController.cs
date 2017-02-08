0ee5678469ffa403156f4b708e5af40f7fdafb9e
Simon Rigby <simon.rigby@levelnis.co.uk>
2017-02-08 13:01:14 +0000

    Added Username API Controller and route

---
+++ Web/Controllers/Api/UsernameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Levelnis.Learning.EndToEndExamples.Web.Controllers.Api
+{
+    public class UsernameController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok("OK");
+        }
+    }
+}
